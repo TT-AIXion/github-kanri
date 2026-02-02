0b937eaf8bc2612e750c1ed6611900f80abad3ec
TT-AIXion <takuto.tanaka@aix-ion.com>
2026-02-02 22:19:44 +0900

    chore: update homebrew formula tag

--- Formula/gkn.rb
+++ Formula/gkn.rb
@@ -1,10 +1,10 @@
 class Gkn < Formula
   desc "GitHub repository management CLI for local operations"
   homepage "https://github.com/TT-AIXion/github-kanri"
-  version "0.0.0-main.20260202T130646Z.7e8e2b1"
+  version "0.0.0-main.20260202T131858Z.7f23932"
   url "https://github.com/TT-AIXion/github-kanri.git",
-      tag: "v0.0.0-main.20260202T130646Z.7e8e2b1",
-      revision: "b5289a0e9cddcd0f2f54503aa35631af43c6b4b1"
+      tag: "v0.0.0-main.20260202T131858Z.7f23932",
+      revision: "2d3ee34ce0228ada0b878cfd9794444dc6b17ee5"
   license "MIT"
 
   depends_on "go" => :build
