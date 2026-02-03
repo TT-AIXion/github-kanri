0e1b5af4bc1470feee8bd210e0baa5a18c58f7d8
TT-AIXion <takuto.tanaka@aix-ion.com>
2026-02-03 11:01:07 +0900

    chore: bump formula v0.0.0-main.20260203T020046Z.95b18f0

--- Formula/gkn.rb
+++ Formula/gkn.rb
@@ -1,10 +1,10 @@
 class Gkn < Formula
   desc "GitHub repository management CLI for local operations"
   homepage "https://github.com/TT-AIXion/github-kanri"
-  version "0.0.0-main.20260203T013101Z.42c3a83"
+  version "0.0.0-main.20260203T020046Z.95b18f0"
   url "https://github.com/TT-AIXion/github-kanri.git",
-      tag: "v0.0.0-main.20260203T013101Z.42c3a83",
-      revision: "42c3a83d1a690f6ad2521bf40ecfd2e4331ee1ab"
+      tag: "v0.0.0-main.20260203T020046Z.95b18f0",
+      revision: "95b18f084c643a5f0839b3e0eef8580a51639d1f"
   license "MIT"
 
   depends_on "go" => :build
